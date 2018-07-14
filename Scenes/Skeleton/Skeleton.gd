b8afeab3e02d13684648b190bc02f5d32acc9247
soekmego <soekmego@users.noreply.github.com>
2018-07-14 18:36:53 +0200

    combo system
    implemented a combo system

--- Scenes/Skeleton/Skeleton.gd
+++ Scenes/Skeleton/Skeleton.gd
@@ -4,10 +4,12 @@ export (int) var speed = 100
 onready var sprite = $Sprite
 
 enum {IDLE, RUN, ROLL, ATTACK1, ATTACK2, ATTACK3}
+
 var direction = Vector2()
 var state
 var anim = ""
 var new_anim
+var combo = false
 
 func _ready():
 	change_state(IDLE)
@@ -35,12 +37,12 @@ func get_input():
 	var roll = Input.is_action_pressed("ui_select")
 	var attack = Input.is_action_pressed("ui_accept")
 	
-	if right and state != ROLL:
+	if right and state == IDLE:
 		change_state(RUN)
 		direction.x += 1
 		sprite.flip_h = false
 	
-	if left and state != ROLL:
+	if left and state == IDLE:
 		change_state(RUN)
 		direction.x -= 1
 		sprite.flip_h = true
@@ -59,12 +61,21 @@ func get_input():
 		match state:
 			IDLE, RUN:
 				change_state(ATTACK1)
-			#ATTACK1:
-			#	change_state(ATTACK2)
-			#ATTACK2:
-			#	change_state(ATTACK3)
+			ATTACK1:
+				if combo == true:
+					change_state(ATTACK2)
+					combo = false
+			ATTACK2:
+				if combo == true:
+					change_state(ATTACK3)
 	
 	direction = direction.normalized() * speed
+
+func combo_enabled():
+	combo = true
+
+func combo_disabled():
+	combo = false
 
 func _process(delta):
 	get_input()
@@ -72,7 +83,6 @@ func _process(delta):
 		anim = new_anim
 		$Animation.play(anim)
 	move_and_slide(direction)
-	#print(state)
 
 func _on_Animation_animation_finished(animation):
 	match animation:
